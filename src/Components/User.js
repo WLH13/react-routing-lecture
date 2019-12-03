120e9d4e9c4fef3ef51217bd1efc399f37d0ad99
Matt Bodily <mattcbodily@gmail.com>
2019-12-03 12:44:10 -0700

    lecture code done

---
+++ src/Components/User.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class User extends Component {
+    constructor(){
+        super()
+        this.state = {
+            users: ['Matt', 'Matias', 'Rob']
+        }
+    }
+
+    render(){
+        console.log(this.props)
+        return(
+            <div>{this.state.users[+this.props.match.params.id]}</div>
+        )
+    }
+}
+
+export default User;
